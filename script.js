0315225877dc7d8422718491a8a5dc7ef6e499f8
Harry Garrood <harry@garrood.me>
2025-12-05 11:44:37 +0000

    more

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-function selectOnChange(event) {
-    const newValue = event.target.value 
+function onTypeSelected(newValue) {
     setDisplay({
         "atk-super-effective": getKeys(2, attackingTypeEffectiveness[newValue]),
         "atk-not-very-effective": getKeys(0.5, attackingTypeEffectiveness[newValue]),
@@ -35,8 +34,6 @@ function getKeys(value, obj) {
     return result
 }
 
-document.getElementById('type-1-select').addEventListener('change', selectOnChange)
-
 const attackingTypeEffectiveness = {
     "Normal": {
         "Rock": 0.5,
@@ -60,7 +57,60 @@ const attackingTypeEffectiveness = {
         "Ground": 2,
         "Rock": 2,
         "Dragon": 0.5,
+    },
+    "Electric": {
+        "Water": 2,
+        "Electric": 0.5,
+        "Grass": 0.5,
+        "Ground": 0,
+        "Flying": 2,
+        "Dragon": 0.5
+    },
+    "Grass": {
+        "Fire": 0.5,
+        "Water": 2,
+        "Grass": 0.5,
+        "Poison": 0.5,
+        "Ground": 2,
+        "Flying": 0.5,
+        "Bug": 0.5,
+        "Rock": 2,
+        "Dragon": 0.5,
+        "Steel": 0.5,
+    },
+    "Ice": {
+        "Fire": 0.5,
+        "Water": 0.5,
+        "Grass": 2,
+        "Ice": 0.5,
+        "Ground": 2,
+        "Flying": 2,
+        "Dragon": 2,
+        "Steel": 0.5,
+    },
+    "Fighting": {
+        "Normal": 2,
+        "Ice": 2,
+        "Poison": 0.5,
+        "Flying": 0.5,
+        "Psychic": 0.5,
+        "Bug": 0.5,
+        "Rock": 2,
+        "Ghost": 0,
+        "Dark": 2,
+        "Steel": 2,
+        "Fairy": 0.5
+    },
+    "Poison": {
+        "Grass": 2,
+        "Poison": 0.5,
+        "Ground": 0.5,
+        "Rock": 0.5,
+        "Ghost": 0.5,
+        "Steel": 0,
+        "Fairy": 2
     }
+
 }
 
 const defendingTypeEffectiveness = (function() {
@@ -72,3 +122,7 @@ const defendingTypeEffectiveness = (function() {
     }
     return result
 })()
+
+const type1Select = document.getElementById('type-1-select')
+type1Select.addEventListener('change', (event) => onTypeSelected(event.target.value))
+onTypeSelected(type1Select.value)
